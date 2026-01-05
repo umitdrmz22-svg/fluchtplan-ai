1d85320eb6f399198ad98b340410a77a81a72256
umitdrmz22-svg <umitdrmz22@gmail.com>
2026-01-05 11:13:16 +0100

    Update generate.js

--- functions/api/generate.js
+++ functions/api/generate.js
@@ -44,4 +44,3 @@ Kontext: ${JSON.stringify(payload)}`;
 function json(obj, status=200){
   return new Response(JSON.stringify(obj), { status, headers: { "Content-Type": "application/json" } });
 }
-``
