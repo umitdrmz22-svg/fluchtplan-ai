dd88eda2c8f69512be50d47c92848def475d8054
umitdrmz22-svg <umitdrmz22@gmail.com>
2026-08-06 12:44:46 +0200

    Produktive Supabase-Verbindung konfigurieren

--- public/cloud-config.js
+++ public/cloud-config.js
@@ -1,9 +1,11 @@
 'use strict';
 (function(){
   const shared=window.EHS_PLATFORM_CONFIG||{};
+  const defaultSupabaseUrl='https://rqvcbjomrjccyuchxpuh.supabase.co';
+  const defaultPublishableKey='sb_publishable_iKh-ZfqV3iJpr_9b7SErEA_XhrqnSsY';
   window.FRP_CLOUD_CONFIG=Object.freeze({
-    supabaseUrl:shared.supabaseUrl||'',
-    supabasePublishableKey:shared.supabasePublishableKey||shared.supabaseAnonKey||'',
+    supabaseUrl:shared.supabaseUrl||defaultSupabaseUrl,
+    supabasePublishableKey:shared.supabasePublishableKey||shared.supabaseAnonKey||defaultPublishableKey,
     appKey:'fluchtplan',
     storageKey:'fluchtplan-studio-v2'
   });
